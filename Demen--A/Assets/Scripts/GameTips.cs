9703144b2393e0dbb78caeadc251b3006602f4dd
RingoRango99 <65308618+RingoRango99@users.noreply.github.com>
2021-05-15 07:25:18 +0100

    Added final textures and models
    added the last textures and models needed to finish off the final version of the game world

--- Demen--A/Assets/Scripts/GameTips.cs
+++ Demen--A/Assets/Scripts/GameTips.cs
@@ -16,7 +16,7 @@ public class GameTips : MonoBehaviour
         Tips[0] = "Tip: Remember, Dementia isn't a single disease, it's an overall term like heart disease.";
         Tips[1] = "Tip: Symptoms for Dementia can be any of the following; Memory loss, Thinking speed problems, Language problems, Misunderstanding things, Misjudging things, Problems with mood and problems with movement.";
         Tips[2] = "Tip: People who suffer from Dementia could also suffer from personality changes and loss of empathy.";
-        Tips[3] = "Tip: If you ever get stuck, loose an item or put the items in the wrong order press T to reset the items.";
+        Tips[3] = "Tip: If you ever get stuck, lose an item or put the items in the wrong order press T to reset the items.";
         Tips[4] = "Tip: Press R to see the recipe that you have to follow.";
         Tips[5] = "Tip: Anyone with Dementia will usually need help from their friends or relatives, inlcuding help with making decisions";
 
